4f7ed24d96dfa735ab1ae04102b32a9e133252fc
Shaifullah Arfan <38496311+Shaif-Arfan@users.noreply.github.com>
2020-03-02 22:41:04 +0600

    initial

---
+++ app.js
@@ -0,0 +1,7 @@
+const hamburger_icon = document.querySelector('#nav-bar .nav-bar .nav-list .hamburger');
+const nav_list = document.querySelector('#nav-bar .nav-bar .nav-list');
+
+hamburger_icon.addEventListener('click', () => {
+	nav_list.classList.toggle('open');
+	hamburger_icon.classList.toggle('active');
+});
